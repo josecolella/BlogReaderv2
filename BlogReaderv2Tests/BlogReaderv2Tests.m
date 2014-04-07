89dafbc05618e42ae57a41a6011a081c92b64f62
josecolella <josecolella@yahoo.com>
2014-04-07 21:27:52 +0200

    Initial Commit

---
+++ BlogReaderv2Tests/BlogReaderv2Tests.m
@@ -0,0 +1,34 @@
+//
+//  BlogReaderv2Tests.m
+//  BlogReaderv2Tests
+//
+//  Created by Jose Colella on 07/04/2014.
+//  Copyright (c) 2014 Jose Colella. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface BlogReaderv2Tests : XCTestCase
+
+@end
+
+@implementation BlogReaderv2Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
